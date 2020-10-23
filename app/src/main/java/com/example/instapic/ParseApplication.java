500c57d1a1477a87d23e3b07dba4bf2506ff4345
Sameeksha22 <sameeksh@usc.edu>
2020-10-22 22:06:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/instapic/ParseApplication.java
@@ -0,0 +1,39 @@
+package com.example.instapic;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("QJWFdViFyvZVTCRtJ7d5jBXRak1sG47rdsIBtiFU")
+                .clientKey("2Eod9tTfRY3ZxAAlWX2NqbBux0DOrh2Yb3uyQATg")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
